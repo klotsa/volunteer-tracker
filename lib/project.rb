dadf0b3dfb151f7a19640ea947cda2619642d59b
Anatoliy Klots <klots@uw.edu>
2017-05-05 10:59:51 -0700

    add save, ==, all testing

--- lib/project.rb
+++ lib/project.rb
@@ -1,20 +1,28 @@
 class Project
-  attr_reader(:id, :task)
+  attr_reader(:id, :name)
 
   def initialize(attributes)
     @id = attributes.fetch(:id)
-    @name = attributes.fetc(:name)
+    @name = attributes.fetch(:name)
   end
 
   def self.all
     all_projects = DB.exec("SELECT * FROM projects;")
     saved_projects = []
     all_projects.each() do |project|
-      id = project.fetch(:id)
-      task = project.fetch(:task)
-      each_project = Project.new({:id => id, :task => task})
-      saved_projects.push(each_project)
+      id = project.fetch("id").to_i
+      name = project.fetch("name")
+      saved_projects.push(Project.new({:id => id, :name => name}))
     end
     saved_projects
+  end
+
+  def save
+    result = DB.exec("INSERT INTO projects (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first().fetch("id").to_i()
+  end
+
+  def ==(test_project)
+    (self.id() == test_project.id()) && (self.name() == test_project.name())
   end
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -1,9 +1,41 @@
-require('spec_helper')
+require("spec_helper")
 
 describe(Project) do
-  describe('.all') do
-    it('starts off with without projects') do
+  describe(".all") do
+    it("starts off with without projects") do
       expect(Project.all()).to(eq([]))
     end
   end
+
+  describe("#name") do
+    it("names the task") do
+      test_project = Project.new({:id => nil, :name => "paint the fence"})
+      expect(test_project.name)
+    end
+  end
+
+  describe("#id") do
+    it("tells the id") do
+      test_project = Project.new({:id => nil, :name => "paint the fence"})
+      test_project.save()
+      expect(test_project.id()).to(be_an_instance_of (Fixnum))
+    end
+  end
+
+  describe("#save") do
+    it ("saves the projects into the db") do
+      test_project = Project.new({:id => 1, :name => "paint the fence"})
+      test_project.save()
+      expect(Project.all()).to(eq([test_project]))
+    end
+  end
+  describe("#==") do
+   it("is the same project if it has the same name + id") do
+     test_project1 = Project.new({:id => nil, :name => "paint the fence"})
+     test_project2 = Project.new({:id => nil, :name => "paint the fence"})
+     expect(test_project1).to eq(test_project2)
+   end
+ end
+
+
 end
